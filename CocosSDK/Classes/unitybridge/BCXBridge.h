65e2473c53db7e39545f37fa3b2ad00254d67876
hugo <hugohuang1111@gmail.com>
2019-09-20 09:38:47 +0800

    fix unity bridge

--- CocosSDK/Classes/unitybridge/BCXBridge.h
+++ CocosSDK/Classes/unitybridge/BCXBridge.h
@@ -8,8 +8,9 @@ extern "C" {
 void BCX_set_unity_callback(void* callback);
 void BCX_reflectionCall(const char* json);
 void BCX_connect(const char* chainId, const char* nodeUrlsString, const char* faucetUrl, const char* coreAsset, bool isOpenLog);
+void BCX_get_version_info();
+void BCX_create_account(const char* account, const char* password, const char* accountType, bool isAutoLogin);
 
-    
 #ifdef __cplusplus
 }
 #endif
--- CocosSDK/Classes/unitybridge/BCXBridge.mm
+++ CocosSDK/Classes/unitybridge/BCXBridge.mm
@@ -8,30 +8,47 @@ BCXBridgeIOS::BCXBridgeListener* bcxBridgeListener = nullptr;
 NSString* dic2str(NSDictionary *dic) {
     NSError *error = nil;
     NSData *jsonData = nil;
-    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-    [dic enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
-        NSString *keyString = nil;
-        NSString *valueString = nil;
-        if ([key isKindOfClass:[NSString class]]) {
-            keyString = key;
-        }else{
-            keyString = [NSString stringWithFormat:@"%@",key];
-        }
-        
-        if ([obj isKindOfClass:[NSString class]]) {
-            valueString = obj;
-        }else{
-            valueString = [NSString stringWithFormat:@"%@",obj];
-        }
-        
-        [dict setObject:valueString forKey:keyString];
-    }];
-    jsonData = [NSJSONSerialization dataWithJSONObject:dict options:0 error:&error];
+    jsonData = [NSJSONSerialization dataWithJSONObject:dic options:0 error:&error];
     if ([jsonData length] == 0 || error != nil) {
         return @"";
     }
     NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
     return jsonString;
+}
+
+NSString* response2str(id responseObject) {
+    NSString* json = @"{}";
+    if (nil == responseObject) {
+        return json;
+    }
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    if ([responseObject isKindOfClass:[NSDictionary class]]) {
+        json = dic2str(responseObject);
+    } else {
+        [dict setObject:[NSNumber numberWithInt:1] forKey: @"code"];
+        if ([responseObject isKindOfClass:[NSArray class]]) {
+            NSArray* arr = responseObject;
+            NSMutableArray *marr = [NSMutableArray array];
+            for (id item in arr) {
+                if (![item isKindOfClass: [NSNull class]]) {
+                    [marr addObject:item];
+                }
+            }
+
+            [dict setObject:marr forKey: @"data"];
+        } else {
+            [dict setObject:responseObject forKey: @"data"];
+        }
+        json = dic2str(dict);
+    }
+    return json;
+}
+
+NSString* NSError2str(NSError* error) {
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    [dict setObject: [NSNumber numberWithInteger: error.code] forKey: @"code"];
+    [dict setObject: error.description forKey: @"data"];
+    return dic2str(dict);
 }
 
 void BCX_set_unity_callback(void* callback) {
@@ -53,18 +70,20 @@ void BCX_reflectionCall(const char* json) {
     }
     NSString* f = [dic valueForKey:@"f"];
     NSArray* p = [dic valueForKey:@"p"];
-    
+    NSString* fsignature = [NSString stringWithFormat:@"%@:", f];
+
     CocosSDK* bcxSDK = [CocosSDK shareInstance];
     int i=0;
     unsigned int mc = 0;
+    bool bHandle = false;
     Method * mlist = class_copyMethodList(object_getClass(bcxSDK), &mc);
     for(i=0; i<mc; i++) {
         Method method = mlist[i];
         SEL mSel = method_getName(method);
         NSString* mName = [NSString stringWithUTF8String: sel_getName(mSel)];
-        if ([mName hasPrefix:f]) {
+        if ([mName hasPrefix:fsignature]) {
             if([bcxSDK respondsToSelector:mSel]) {
-                // NSUInteger argsCount = method_getNumberOfArguments(method);
+                //unsigned int argsCount = method_getNumberOfArguments(method);
                 char returnType;
                 method_getReturnType(method, &returnType, 1);
 
@@ -75,10 +94,13 @@ void BCX_reflectionCall(const char* json) {
                 if (nullptr != p) {
                     for (idx = 0; idx < [p count]; idx++) {
                         id param = p[idx];
-                        if ([param isKindOfClass:[NSString class]]) {
+                        if ([param isKindOfClass: [NSArray class]]) {
                             [inv setArgument:&param atIndex:idx + 2];
-                        } else if ([param isKindOfClass:[NSNumber class]]) {
+                        } else if ([param isKindOfClass: [NSString class]]) {
                             [inv setArgument:&param atIndex:idx + 2];
+                        } else if ([param isKindOfClass: [NSNumber class]]) {
+                            NSInteger nsint = ((NSNumber*)param).integerValue;
+                            [inv setArgument:&nsint atIndex:idx + 2];
                         } else {
                             NSLog(@"Unknown param type:%@", [param class]);
                         }
@@ -88,25 +110,14 @@ void BCX_reflectionCall(const char* json) {
                     SuccessBlock sucBlock = nil;
                     Error errBlock = nil;
                     sucBlock = ^(id responseObject){
-                        NSString* json = @"";
-                        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-                        if ([responseObject isKindOfClass:[NSDictionary class]]) {
-                            json = dic2str(responseObject);
-                        } else {
-                            [dict setObject:[NSNumber numberWithInt:1] forKey: @"code"];
-                            [dict setObject:responseObject forKey: @"data"];
-                            json = dic2str(dict);
-                        }
+                        NSString* json = response2str(responseObject);
                         NSLog(@"success block:%@ => %@", f, json);
                         bcxBridgeListener->notifyUnity(f.UTF8String, json.UTF8String);
                         if (nil != sucBlock) Block_release(sucBlock);
                         if (nil != errBlock) Block_release(errBlock);
                     };
                     errBlock = ^(NSError *error){
-                        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-                        [dict setObject:[NSNumber numberWithInt:0] forKey: @"code"];
-                        [dict setObject:error.description forKey: @"data"];
-                        NSString* json = dic2str(dict);
+                        NSString* json = NSError2str(error);
                         NSLog(@"error block:%@ => %@", f, json);
                         bcxBridgeListener->notifyUnity(f.UTF8String, json.UTF8String);
                         if (nil != sucBlock) Block_release(sucBlock);
@@ -120,11 +131,15 @@ void BCX_reflectionCall(const char* json) {
 
                 [inv retainArguments];
                 [inv invoke];
+                bHandle = true;
                 break;
             }
         }
     }
     free(mlist);
+    if (!bHandle) {
+        NSLog(@"ERROR, %@ not invoke", f);
+    }
 }
 
 void BCX_connect(const char* chainId,
@@ -148,8 +163,88 @@ void BCX_connect(const char* chainId,
                                            } else {
                                                [dict setObject:[NSNumber numberWithInt:0] forKey: @"code"];
                                            }
-                                           NSLog(@"cb:connect");
                                            bcxBridgeListener->notifyUnity("connect", dic2str(dict).UTF8String);
                                        }];
+    NSLog(@"BCX iOS SDK Version: %@", [CocosSDK shareInstance].Cocos_SdkCurentVersion);
 }
+
+void BCX_get_version_info() {
+    if (nullptr == bcxBridgeListener) {
+        return;
+    }
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    [dict setObject: [NSNumber numberWithInt:1] forKey: @"code"];
+    [dict setObject: [CocosSDK shareInstance].Cocos_SdkCurentVersion forKey: @"data"];
+    bcxBridgeListener->notifyUnity("get_version_info", dic2str(dict).UTF8String);
+}
+
+void BCX_create_account(const char* account, const char* password, const char* accountType, bool isAutoLogin) {
+    if (nullptr == bcxBridgeListener) {
+        return;
+    }
+    NSString* accType = [NSString stringWithUTF8String:accountType];
+    CocosWalletMode m = CocosWalletModeAccount;
+    if (NSOrderedSame == [@"WALLET" caseInsensitiveCompare:accType]) {
+        m = CocosWalletModeWallet;
+    }
+    [[CocosSDK shareInstance] Cocos_CreateAccountWalletMode: m
+                                                AccountName: [NSString stringWithUTF8String:account]
+                                                   Password: [NSString stringWithUTF8String:password]
+                                                  AutoLogin: isAutoLogin
+                                                    Success: ^(id responseObject) {
+                                                        bcxBridgeListener->notifyUnity("create_account", response2str(responseObject).UTF8String);
+                                                    }
+                                                      Error: ^(NSError *error) {
+                                                          bcxBridgeListener->notifyUnity("create_account", NSError2str(error).UTF8String);
+                                                      }];
+}
+
+
+/*
+void BCX_calculate_invoking_contract_fee(const char* account,
+                                         const char* feeAssetSymbol,
+                                         const char* contractId,
+                                         const char* functionName,
+                                         const char* params) {
+    if (nullptr == bcxBridgeListener) {
+        return;
+    }
+    NSString* nsParams = [NSString stringWithUTF8String:params];
+    [[CocosSDK shareInstance] Cocos_GetCallContractFee: [NSString stringWithUTF8String:contractId]
+                                   ContractMethodParam: [nsParams componentsSeparatedByString: @","]
+                                        ContractMethod: [NSString stringWithUTF8String:functionName]
+                                         CallerAccount: [NSString stringWithUTF8String:account]
+                                        feePayingAsset: [NSString stringWithUTF8String:feeAssetSymbol]
+                                               Success: ^(id responseObject) {
+                                                   bcxBridgeListener->notifyUnity("calculate_invoking_contract_fee", response2str(responseObject).UTF8String);
+                                               }
+                                                 Error: ^(NSError *error) {
+                                                     bcxBridgeListener->notifyUnity("calculate_invoking_contract_fee", NSError2str(error).UTF8String);
+                                                 }];
+}
+
+void BCX_invoking_contract(const char* account,
+                           const char* password,
+                           const char* feeAssetSymbol,
+                           const char* contractId,
+                           const char* functionName,
+                           const char* params) {
+    if (nullptr == bcxBridgeListener) {
+        return;
+    }
+    NSString* nsParams = [NSString stringWithUTF8String:params];
+    [[CocosSDK shareInstance] Cocos_CallContract: [NSString stringWithUTF8String:contractId]
+                             ContractMethodParam: [nsParams componentsSeparatedByString: @","]
+                                  ContractMethod: [NSString stringWithUTF8String:functionName]
+                                   CallerAccount: [NSString stringWithUTF8String:account]
+                                  feePayingAsset: [NSString stringWithUTF8String:feeAssetSymbol]
+                                        Password: [NSString stringWithUTF8String:password]
+                                         Success: ^(id responseObject) {
+                                             bcxBridgeListener->notifyUnity("invoking_contract", response2str(responseObject).UTF8String);
+                                         }
+                                           Error: ^(NSError *error) {
+                                               bcxBridgeListener->notifyUnity("invoking_contract", NSError2str(error).UTF8String);
+                                           }];
+}
+*/
 
